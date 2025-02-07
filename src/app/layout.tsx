96fc1054fa4be6bdf1d28b0e9c9e166a3c320b09
Erskine Duenas <erskine.duenas@awsys-i.com>
2025-02-07 15:09:52 +0800

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+  display: "swap",
+  weight: "variable",
+});
+
+export const metadata: Metadata = {
+  title: "Airbnb Clone - ED",
+  description: "Clone Airbnb using NextJs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${nunito.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
