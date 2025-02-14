2d096389bafc8caa15d243621df9b86875bcfc93
Erskine Duenas <erskine.duenas@awsys-i.com>
2025-02-14 08:45:08 +0800

    Done: Github register/sign-up function

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -3,16 +3,20 @@
 import Image from "next/image";
 import React from "react";
 
-function Avatar() {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       height="30"
       width="30"
       alt="Avatar"
-      src="/Images/placeholder.jpg"
+      src={src || "/Images/placeholder.jpg"}
     />
   );
-}
+};
 
 export default Avatar;
--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -13,6 +13,7 @@ import Heading from "../Heading";
 import Input from "@/app/components/inputs/Input";
 import { toast } from "react-hot-toast";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const RegisterModal = useRegisterModal();
@@ -90,7 +91,7 @@ const RegisterModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
